ebded5da7efc577f5fab996f1e3ce9364038ba13
kent-and <kent-and@math.uio.no>
2023-12-20 15:31:36 +0100

    update

--- poisson_ista.py
+++ poisson_ista.py
@@ -28,7 +28,7 @@ def sigma_max_and_AA(A):
 
 def boundary(x, on_boundary): return on_boundary
 
-def make_matrix(N, p, obs_indices): 
+def make_mesh_and_matrix(N, p, obs_indices): 
     mesh = UnitIntervalMesh(N) 
     V = FunctionSpace(mesh, "CG", p) 
     bc = DirichletBC(V, Constant(0), boundary) 
@@ -40,7 +40,7 @@ def make_matrix(N, p, obs_indices):
     A, _ = assemble_system(a, L, bc) 
     invA = np.linalg.inv(A.array())
     obs = invA[indices,:]
-    return obs 
+    return obs, mesh 
 
 
 
@@ -49,7 +49,11 @@ p = 1
 index = int(N/2)
 indices = [index]
 
-A = make_matrix(N, p, indices) 
+A, mesh = make_mesh_and_matrix(N, p, indices) 
+
+x = mesh.coordinates()[:]
+
+print (x) 
 
 print (A)
 print (np.min(A), np.max(A)) 
@@ -60,33 +64,41 @@ print ("sigma ", sigma)
 
 nn = A.shape[1]
 mm = A.shape[0]
-x = np.random.random(nn)
-xp = np.random.random(nn)
+x = np.random.random(nn) * 0 
+xp = np.random.random(nn) * 0 
 y = np.ones(mm)
-#lmbda = A.flatten()
 #print (lmbda) 
 eps = 1e-3 
 lmbda = eps * np.ones(nn) / nn 
+
+lmbda = A.flatten() #- 1e-8 
+lmbda = np.power(10*(x-0.5), 4)
+
+
 
 
 it = 0 
 max_it = 50000 
 residuals = []
 L1_norm_weigheds = []
-while it <= max_it:  
-    z = xp - 1.0/(sigma* nn)*(np.dot(AA, xp) - np.dot(A.transpose(), y))
+gamma = 1.0e-6
+update = 1.0 
+while it <= max_it and update > gamma:  
+    z = xp - 1.0/(sigma)*(np.dot(AA, xp) - np.dot(A.transpose(), y))
     x = S(z, lmbda/sigma)   
     residual = np.dot(A, x) - y 
     residuals.append(np.linalg.norm(residual)) 
     L1_norm_weighed = np.inner(np.abs(lmbda), np.abs(x)) 
     L1_norm_weigheds.append(L1_norm_weighed) 
-    print ("norm of residual, L1 norm, update ", np.linalg.norm(residual),  np.linalg.norm(x-xp), L1_norm_weighed) 
+    update = np.linalg.norm(x-xp)
+    print ("it# ", it, "  norm of residual, L1 norm, update ", np.linalg.norm(residual),  update, L1_norm_weighed) 
     print (np.linalg.norm(A,2))
     xp[:] = x[:]
     it = it+1 
 
 
 
+print ("min/max solution ", np.min(x), np.max(x))
 
 plt.plot(x) 
 plt.show()
@@ -96,4 +108,6 @@ plt.loglog(L1_norm_weigheds)
 plt.legend(["equation residual ", "L1 comp"])
 plt.show()
 
+plt.plot(A.flatten()) 
+plt.show()
 
